364cab2c99d5064e94284c6cc5796f4c1b384407
NourAlDev <nouralsamawi11.11@gmail.com>
2026-02-07 02:43:30 +0300

    Make activities route dynamic

--- app/api/activities/route.ts
+++ app/api/activities/route.ts
@@ -3,6 +3,8 @@ import dbConnect from "@/lib/db";
 import Activity from "@/models/Activity";
 import { getRecentMemoryActivities } from "@/lib/activity";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
     try {
         await dbConnect();
